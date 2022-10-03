6c0805a1fd6bee9f61b6bc88e94bf30b70e97165
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2022-10-03 18:10:56 +0200

    Add `0014`
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

--- leetcode/problems/all_topics/0014_longest_common_prefix.py
+++ leetcode/problems/all_topics/0014_longest_common_prefix.py
@@ -1,3 +1,4 @@
+from all_import import *
 
 '''
 Write a function to find the longest common prefix string amongst an array of strings.
@@ -21,4 +22,27 @@ Constraints:
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pass
+        ls = len(strs)
+        if ls == 1:
+            return strs[0]
+        prefix = ''
+        pos = 0
+        while True:
+            try:
+                current = strs[0][pos]
+            except IndexError:
+                break
+            index = 1
+            while index < ls:
+                try:
+                    if strs[index][pos] != current:
+                        break
+                except IndexError:
+                    break
+                index += 1
+            if index == ls:
+                prefix = prefix + current
+            else:
+                break
+            pos += 1
+        return prefix
