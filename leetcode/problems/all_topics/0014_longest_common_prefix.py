6347e46e496ce46ba25d594a9ad6e12c9da3ffb1
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-10-03 10:54:33 +0200

    Add `0014` vertical solution
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- leetcode/problems/all_topics/0014_longest_common_prefix.py
+++ leetcode/problems/all_topics/0014_longest_common_prefix.py
@@ -21,4 +21,11 @@ Constraints:
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pass
+        if len(strs) == 0 or strs == None: return ""
+        
+        for i in range(len(strs)):
+            prefix = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or strs[j][i] != prefix:
+                    return strs[0][0:i]
+        return strs[0]
