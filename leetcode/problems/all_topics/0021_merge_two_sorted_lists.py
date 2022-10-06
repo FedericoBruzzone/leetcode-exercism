d67a0c879cf9fcce2d2e22a7ae338316b3a4b26f
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2022-10-06 11:04:11 +0200

    Add `0021`
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

--- leetcode/problems/all_topics/0021_merge_two_sorted_lists.py
+++ leetcode/problems/all_topics/0021_merge_two_sorted_lists.py
@@ -27,10 +27,27 @@ Constraints:
 '''
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+        head = temp = ListNode(-1)
+        
+        while list1 is not None and list2 is not None:
+            if list1.val <= list2.val:
+                temp.next = list1
+                list1 = list1.next
+            else:
+                temp.next = list2
+                list2 = list2.next
+            temp = temp.next
+        
+        if list1 is not None:
+            temp.next = list1
+        if list2 is not None:
+            temp.next = list2
+        
+        return head.next
