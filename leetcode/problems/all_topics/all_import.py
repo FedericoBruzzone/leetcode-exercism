aa2fc8bff7f2a8962b41c2e0fbe33f162d0fbfad
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2022-12-15 17:28:56 +0100

    Add `all_import` file
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

---
+++ leetcode/problems/all_topics/all_import.py
@@ -0,0 +1,17 @@
+
+#import all_import
+# from all_import import *
+
+# import typing
+# import functools
+# from typing import *
+# from functools import *
+
+from typing import List
+from typing import Optional
+from typing import Dict
+from typing import Union
+from typing import Tuple
+
+from math import *
+from queue import PriorityQueue
